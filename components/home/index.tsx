0af99c864a3d5964b8369bba8c92a8b7fa69b934
luotao <luotao_t@yeah.net>
2024-05-24 00:26:03 +0800

    feat: dataType describe

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,5 +1,17 @@
 const Home = () => {
-  return <div>个人手记</div>
+  return <div className="pt-[40px]">
+    <h2
+      className="text-center bg-clip-text text-[60px] font-bold font-luckiest tracking-[10px]"
+      style={{
+        backgroundImage: "-webkit-linear-gradient(135deg, #FEB692 10%, #EA5455 100%)",
+        WebkitTextFillColor: "transparent"
+      }}
+    >
+      Learning
+    </h2>
+
+    <p className="text-center text-[24px] font-luckiest">Dream in the heart, action under the feet.</p>
+  </div>
 }
 
 export default Home
